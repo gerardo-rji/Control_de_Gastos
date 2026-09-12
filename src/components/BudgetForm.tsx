3152fc5995d9a09fe3d8e58681615ac438e44f18
Gerardo_RJI <gera.rji96@gmail.com>
2026-09-12 04:02:20 -0600

    Add budget context and reducer for global state management

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,9 +1,11 @@
-import {useMemo, useState} from "react";
+import {useMemo, useState, type SubmitEvent} from "react";
 import type { ChangeEvent } from "react";
+import { useBudget } from "../hooks/useBudget.ts";
 
 export default function BudgetForm() {
 
   const [budget, setBudget] = useState(0);
+  const { dispatch } = useBudget();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setBudget(+e.target.value)
@@ -11,9 +13,15 @@ export default function BudgetForm() {
 
   const isValid = useMemo(() => isNaN(budget) || budget <= 0, [budget])
 
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    dispatch({type: 'add-budget', payload: {budget}})
+  }
+
   return (
     <>
-      <form className="space-y-5">
+      <form className="space-y-5" onSubmit={handleSubmit}>
         <div className="flex flex-col space-y-5">
           <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
             Definir Presupuesto
