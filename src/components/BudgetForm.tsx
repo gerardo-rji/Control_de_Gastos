38ef654ed17f18be6bbab4319a9e91f0e1d09d26
Gerardo_RJI <gera.rji96@gmail.com>
2026-09-12 00:02:38 -0600

    Add budget form with state management and validation

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,7 +1,41 @@
+import {useMemo, useState} from "react";
+import type { ChangeEvent } from "react";
+
 export default function BudgetForm() {
+
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(+e.target.value)
+  }
+
+  const isValid = useMemo(() => isNaN(budget) || budget <= 0, [budget])
+
   return (
     <>
-      <h1>Budget Form</h1>
+      <form className="space-y-5">
+        <div className="flex flex-col space-y-5">
+          <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+            Definir Presupuesto
+          </label>
+          <input
+            id="budget"
+            type="number"
+            className="w-full bg-white border border-gray-200 p-2"
+            placeholder="Define tu presupuesto"
+            name="budget"
+            value={budget}
+            onChange={handleChange}
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Definir Presupuesto"
+          className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+          disabled={isValid}
+        />
+      </form>
     </>
   );
 }
